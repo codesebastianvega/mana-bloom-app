bf467093ad109097a92efaf38e3d874a993a0877
Juan Sebastian Vega <106327470+codesebastianvega@users.noreply.github.com>
2025-08-10 14:45:14 -0500

    feat: add gradient and pressed state to FAB

--- src/components/AddTaskButton/AddTaskButton.js
+++ src/components/AddTaskButton/AddTaskButton.js
@@ -1,15 +1,31 @@
 // src/components/AddTaskButton/AddTaskButton.js
 
-import React from "react";
+import React, { useState } from "react";
 import { TouchableOpacity } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import { FontAwesome } from "@expo/vector-icons";
 import styles from "./AddTaskButton.styles";
 import { Colors } from "../../theme";
 
 export default function AddTaskButton({ onPress }) {
+  const [isPressed, setIsPressed] = useState(false);
   return (
-    <TouchableOpacity style={styles.fab} onPress={onPress}>
-      <FontAwesome name="plus" size={24} color={Colors.background} />
+    <TouchableOpacity
+      style={[styles.fab, isPressed && styles.fabShadowPressed]}
+      onPress={onPress}
+      onPressIn={() => setIsPressed(true)}
+      onPressOut={() => setIsPressed(false)}
+    >
+      <LinearGradient
+        colors={[
+          Colors.primaryFantasy,
+          Colors.secondaryFantasy,
+          Colors.tertiaryFantasy,
+        ]}
+        style={styles.gradient}
+      >
+        <FontAwesome name="plus" size={24} color={Colors.background} />
+      </LinearGradient>
     </TouchableOpacity>
   );
 }
