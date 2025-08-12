346f633f1b24ae39534c725fd472d5ac1c5c6917
Juan Sebastian Vega <106327470+codesebastianvega@users.noreply.github.com>
2025-08-12 14:28:41 -0500

    feat: add bottom navigation footer

--- src/components/Footer.styles.js
+++ src/components/Footer.styles.js
@@ -1,59 +1,41 @@
-// src/components/Footer/Footer.styles.js
-
 import { StyleSheet } from "react-native";
-import { Colors, Spacing } from "../theme";
+import { Spacing } from "../theme";
 
 export default StyleSheet.create({
   container: {
     position: "absolute",
+    bottom: 0,
     left: 0,
     right: 0,
-    bottom: 0,
     flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "flex-start",
-    backgroundColor: "rgba(34, 35, 54, 1)",
-    paddingVertical: Spacing.small,
-    paddingHorizontal: Spacing.base,
-    borderTopWidth: 2,
-    borderTopColor: "rgba(255, 255, 255, 0.1)",
-    shadowColor: Colors.shadow,
-    shadowOffset: { width: 0, height: -2 },
-    shadowOpacity: 0.8,
-    shadowRadius: 10,
-    elevation: 5,
-    borderRadius: 10,
-    paddingTop: Spacing.tiny,
-    paddingBottom: 15,
+    backgroundColor: "#1b1231",
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
   },
-  button: {
+  touchable: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    paddingVertical: Spacing.small,
   },
   activeButton: {
-    transform: [{ scale: 1.2 }], // Escala sutilmente el botón activo
-    transition: "transform 0.3s ease-in-out", // Animación de escala
-  },
-  iconWrapper: {
-    padding: Spacing.small,
-    borderRadius: 50,
-    justifyContent: "center",
     alignItems: "center",
-    // Ya no hay un estilo de fondo para el estado activo aquí.
+    justifyContent: "center",
+    paddingVertical: Spacing.small,
+    borderRadius: 20,
   },
-  // El estilo activeIconWrapper ha sido eliminado.
+  inactiveButton: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: Spacing.small,
+  },
   label: {
     marginTop: Spacing.tiny,
     fontSize: 12,
-    fontWeight: "600",
-    color: Colors.text,
-    opacity: 0.8,
+    color: "#A9A9A9",
   },
   activeLabel: {
-    color: Colors.text,
+    marginTop: Spacing.tiny,
+    fontSize: 12,
+    color: "#FFF",
     fontWeight: "bold",
-    opacity: 1,
   },
 });
+
